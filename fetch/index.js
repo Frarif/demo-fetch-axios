a019cbde7a3377fe44a5003a8f86ad948a6b26c3
Frarif <freakinarif25@gmail.com>
2023-02-12 09:25:49 +0700

    create: patchData function

--- fetch/index.js
+++ fetch/index.js
@@ -1,8 +1,10 @@
 const BASE_API_URL =
   "https://api.kontenbase.com/query/api/v1/3fdc905a-bd85-4a3b-92c8-18e1efc7fa40";
 
+const API_URL = `${BASE_API_URL}/posts`;
+
 async function getPosts() {
-  const response = await fetch(`${BASE_API_URL}/posts`);
+  const response = await fetch(API_URL);
   const posts = await response.json();
   console.log(posts);
 }
@@ -13,7 +15,7 @@ async function addNewPosts() {
     story: "Belajar buat hari minggu",
   };
 
-  const response = await fetch(`${BASE_API_URL}/posts`, {
+  const response = await fetch(API_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
@@ -22,4 +24,20 @@ async function addNewPosts() {
   console.log(newPosts);
 }
 
-getPosts();
+async function patchData() {
+  const data = {
+    titles: "Session 16",
+    story: "Pembagian kelompok buat project akhir.",
+  };
+
+  const response = await fetch(`${API_URL}/63e79f338185f56900e351ce`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  const resultPatch = await response.json();
+  console.log(resultPatch);
+}
+
+patchData();
